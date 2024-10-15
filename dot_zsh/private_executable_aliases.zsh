f1579a963e5d2557e15f7bc9060c8bc3b4ea7254
Mohammad Ataei <mammadataei@gmail.com>
2024-10-15 16:00:40 +0330

    Add .zsh/aliases.zsh

---
+++ dot_zsh/private_executable_aliases.zsh
@@ -0,0 +1,14 @@
+# Navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+alias l='ls -lah'
+alias c='clear'
+alias cat='bat'
+alias dots="chezmoi"
+
+alias libinit="pnpm dlx degit mammadataei/libinit"
+
+alias art="php artisan"
